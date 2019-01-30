334c10826eca8d8fce09be7ac5ff595d17b941df
odan <odan3240@gmail.com>
2019-01-30 23:45:15 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import os from "os";
 import express from "express";
 
 const app = express();
@@ -6,6 +7,6 @@ const server = app.listen(3000, "0.0.0.0", () => {
   console.log("start listen", server.address());
 });
 
-app.get("/", (_, res) => {
-  res.json("hello");
+app.get("/", (req, res) => {
+  res.json([req.ip, req.hostname, os.hostname()]);
 });
